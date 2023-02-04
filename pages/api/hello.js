805d96836edcd9e77345a81899fe255b9a4c7441
nyan-tama <shake.flake.love@gmail.com>
2023-02-04 23:29:07 +0900

    add axios

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe', res: res })
+  if (req.method === 'POST') {
+    // POSTリクエストを処理します
+    res.status(200).json({ name: 'John Doe', req: req.json() })
+  } else {
+    // その他のHTTPメソッドを処理します
+    return res.status(405).end()
+  }
 }
+
